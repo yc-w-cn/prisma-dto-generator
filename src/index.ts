94eec6f87a94f1974c9a8b1c75259175f7cfdcac
joydragon_wang <joydragon_wang@noreply.gitcode.com>
2025-11-15 00:56:55 +0800

    feat: 动态获取package.json中的版本号替换硬编码

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper';
 
 import { parseConfig } from './core/config';
 import { toModelDescriptors } from './core/dmmf';
 import { emitAll } from './writer/emitter';
 
+// 动态获取版本号
+function getVersion(): string {
+  const packageJsonPath = path.resolve(__dirname, '../package.json');
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+  return packageJson.version;
+}
+
 generatorHandler({
   onManifest() {
     return {
-      version: '0.1.0',
+      version: getVersion(),
       defaultOutput: './generated/dto',
       prettyName: 'Prisma DTO Generator (Swagger)',
     };
